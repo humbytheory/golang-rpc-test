363dd0965ba275129a25ad6c6c10d59193d0a52b
humbythoery <humbytheory@gmail.com>
2015-01-14 06:01:01 -0500

    clean up

--- client.go
+++ client.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-        "crypto/tls"
-        "github.com/docopt/docopt-go"
-        "log"
-        "net/rpc/jsonrpc"
-        "os"
+	"crypto/tls"
+	"github.com/docopt/docopt-go"
+	"log"
+	"net/rpc/jsonrpc"
+	"os"
 )
 
 func main() {
-        usage := `
+	usage := `
 Usage:
-        client [--d] [--c=<config>] CLIENT POLICY SCHEDULE SCHEDULETYPE STATUS STREAM PATHNAME
+        client [--t] [--c=<config>] CLIENT POLICY SCHEDULE SCHEDULETYPE STATUS STREAM PATHNAME
         client --p
 
 Options:
   -h --help
   -c,--c=<config>   JSON configuration file [default: clientConfig.json]
   -p,--p            Print out sample clientConfig.json
-  -d,--d            Dry run`
+  -t,--t            Test run`
 
-        arguments, _ := docopt.Parse(usage, nil, true, "", false)
+	arguments, _ := docopt.Parse(usage, nil, true, "", false)
 
-        // exit if we are only showing a sample config
-        if arguments["--p"].(bool) {
-                b := []byte(`{"TLSCommonCA":"./certs/CA.crt","TLSMyCert":"./certs/boxname.crt","TLSMyKey":"./certs/boxname.key","ServerIP": "192.168.0.2","ServerPort":8075,"ClientIP":"192.168.0.3"}`)
-                PrintSampleConfig(b)
-                return
-        }
+	// exit if we are only showing a sample config
+	if arguments["--p"].(bool) {
+		b := []byte(`{"TLSCommonCA":"./certs/CA.crt","TLSMyCert":"./certs/boxname.crt","TLSMyKey":"./certs/boxname.key","ServerIP": "192.168.0.2","ServerPort":8075,"ClientIP":"192.168.0.3"}`)
+		PrintSampleConfig(b)
+		return
+	}
 
-        // Define the Params for the RPC call
-        var args Args
-        args.Client = arguments["CLIENT"].(string)
-        args.Policy = arguments["POLICY"].(string)
-        args.Schedule = arguments["SCHEDULE"].(string)
-        args.SchedType = arguments["SCHEDULETYPE"].(string)
-        args.Status = arguments["STATUS"].(string)
-        args.Stream = arguments["STREAM"].(string)
-        args.Path = arguments["PATHNAME"].(string)
-        args.DryRun = arguments["--d"].(bool)
+	// Define the Params for the RPC call
+	var args Args
+	args.Client = arguments["CLIENT"].(string)
+	args.Policy = arguments["POLICY"].(string)
+	args.Schedule = arguments["SCHEDULE"].(string)
+	args.SchedType = arguments["SCHEDULETYPE"].(string)
+	args.Status = arguments["STATUS"].(string)
+	args.Stream = arguments["STREAM"].(string)
+	args.Path = arguments["PATHNAME"].(string)
+	args.DryRun = arguments["--t"].(bool)
 
-        // Read configuration from json file and set defaults
-        Settings := ParseConfig(arguments["--c"].(string))
+	// Read configuration from json file and set defaults
+	Settings := ParseConfig(arguments["--c"].(string))
 
-        // Setup TLS and tslConnect
-        config := MustGetTlsConfiguration(Settings.TLSCommonCA, Settings.TLSMyCert, Settings.TLSMyKey)
-        tslConn, err := tls.Dial("tcp", Settings.ServerIPPort, config)
-        defer tslConn.Close()
-        if err != nil {
-                log.Fatal("Error dialing:", err)
-        }
-        err = tslConn.Handshake()
-        if err != nil {
-                log.Fatal("Failed handshake:%v\n", err)
-        }
+	// Setup TLS and tslConnect
+	config := MustGetTlsConfiguration(Settings.TLSCommonCA, Settings.TLSMyCert, Settings.TLSMyKey)
+	tslConn, err := tls.Dial("tcp", Settings.ServerIPPort, config)
+	defer tslConn.Close()
+	if err != nil {
+		log.Fatal("Error dialing:", err)
+	}
+	err = tslConn.Handshake()
+	if err != nil {
+		log.Fatal("Failed handshake:%v\n", err)
+	}
 
-        // Do a synchronous call
-        var reply Results
-        client := jsonrpc.NewClient(tslConn)
-        defer client.Close()
-        err = client.Call("RPCMethods.DoSomething", args, &reply)
-        if err != nil {
-                log.Fatal("Connection refused")
-        }
-        os.Exit(reply.Code)
+	// Do a synchronous call
+	var reply Results
+	client := jsonrpc.NewClient(tslConn)
+	defer client.Close()
+	err = client.Call("RPCMethods.DoSomething", args, &reply)
+	if err != nil {
+		log.Fatal("Connection refused")
+	}
+	os.Exit(reply.Code)
 }
--- server.go
+++ server.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-        //"crypto/x509"
-        "bytes"
-        "crypto/tls"
-        "github.com/docopt/docopt-go"
-        "log"
-        "net"
-        "net/rpc"
-        "net/rpc/jsonrpc"
-        "os/exec"
-        "regexp"
-        "unicode"
+	"bytes"
+	"crypto/tls"
+	"github.com/docopt/docopt-go"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os/exec"
+	"regexp"
+	"unicode"
 )
 
 // RPC status codes to sent to client
@@ -24,173 +23,186 @@ const ErrorDryRun = 4
 var global_ExternalCMDPath string
 var global_ExternalCMD string
 var global_DryRun bool
+var global_DebugLevel string
 
 // RPC method struct
 type RPCMethods struct{}
 
 // Used by RPC method to run external command
 func shellOut(what string) int {
-        log.Printf("[%v] c:%s p:%s", what, global_ExternalCMDPath, global_ExternalCMD)
-        cmd := exec.Command("echo", "-n", "wakka")
-        cmd.Dir = global_ExternalCMDPath
-        var out bytes.Buffer
-        cmd.Stdout = &out
-        if err := cmd.Run(); err != nil {
-                return ErrorExternal
-        }
-        // log.Println(cmd.Stdout)
-        return ErrorNone
+	if global_DebugLevel == "2" {
+		log.Printf("RPC: [%v] c:%s p:%s", what, global_ExternalCMDPath, global_ExternalCMD)
+	}
+	cmd := exec.Command("echo", "-n", "wakka")
+	cmd.Dir = global_ExternalCMDPath
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		return ErrorExternal
+	}
+	// log.Println(cmd.Stdout)
+	return ErrorNone
 }
 
 // RPC method that will be called by client
 func (t *RPCMethods) DoSomething(args *Args, reply *Results) error {
-        log.Println(args)
-        if global_DryRun || args.DryRun {
-                reply.Code = ErrorDryRun
-                return nil
-        }
-        if isValidInput(args.Client) {
-                reply.Code = shellOut(args.Client)
-        } else {
-                reply.Code = ErrorInvalidInput
-        }
-        return nil
+	if global_DebugLevel == "2" {
+		log.Printf("RPC: args: %v  serverDryRun: %v\n", args, global_DryRun)
+	}
+	if global_DryRun || args.DryRun {
+		reply.Code = ErrorDryRun
+		return nil
+	}
+	if isValidInput(args.Client) {
+		reply.Code = shellOut(args.Client)
+	} else {
+		reply.Code = ErrorInvalidInput
+	}
+	return nil
 }
 
 // Used by RPM method to report if input data is valid
 func isValidInput(input string) bool {
-        for _, character := range input {
-                if character > unicode.MaxASCII || !unicode.IsPrint(character) {
-                        return false
-                }
-        }
-        re := regexp.MustCompile("^[a-zA-Z0-9_-]*$")
-        if re.MatchString(input) {
-                return true
-        } else {
-                return false
-        }
+	for _, character := range input {
+		if character > unicode.MaxASCII || !unicode.IsPrint(character) {
+			return false
+		}
+	}
+	re := regexp.MustCompile("^[a-zA-Z0-9_-]*$")
+	if re.MatchString(input) {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
-        usage := `
+	usage := `
 Usage:
-        server [--d] [--c=<config>]
+        server [--t] [--d=<level>] [--c=<config>]
         server --p
 
 Options:
   -h --help
   -c,--c=<config>   JSON configuration file [default: serverConfig.json]
   -p,--p            Print out sample serverConfig.json
-  -d,--d            Dry run`
+  -d,--d=<level>    Debug level 1-3. [default: 0]
+  -t,--t            Test run`
 
-        arguments, _ := docopt.Parse(usage, nil, true, "", false)
+	arguments, _ := docopt.Parse(usage, nil, true, "", false)
 
-        // Exit if we are only showing a sameple config
-        if arguments["--p"].(bool) {
-                b := []byte(`{"TLSCommonCA":"./certs/CA.crt","TLSMyCert":"./certs/boxname.crt","TLSMyKey":"./certs/boxname.key","ServerIP": "192.168.0.2","ServerPort":8075,"ClientIP":"192.168.0.3","ExternalCMDPath":"/tmp","ExternalCMD":"ls"}`)
-                PrintSampleConfig(b)
-                return
-        }
+	// Exit if we are only showing a sameple config
+	if arguments["--p"].(bool) {
+		b := []byte(`{"TLSCommonCA":"./certs/CA.crt","TLSMyCert":"./certs/boxname.crt","TLSMyKey":"./certs/boxname.key","ServerIP": "192.168.0.2","ServerPort":8075,"ClientIP":"192.168.0.3","ExternalCMDPath":"/tmp","ExternalCMD":"ls"}`)
+		PrintSampleConfig(b)
+		return
+	}
 
-        // Read configuration from json file and set defaults
-        Settings := ParseConfig(arguments["--c"].(string))
+	// Read configuration from json file and set defaults
+	Settings := ParseConfig(arguments["--c"].(string))
 
-        global_ExternalCMDPath = Settings.ExternalCMDPath
-        global_ExternalCMD = Settings.ExternalCMD
-        global_DryRun = arguments["--d"].(bool)
+	global_ExternalCMDPath = Settings.ExternalCMDPath
+	global_ExternalCMD = Settings.ExternalCMD
+	global_DryRun = arguments["--t"].(bool)
+	global_DebugLevel = arguments["--d"].(string)
 
-        configIpList := make(map[string]bool)
-        configIpList[Settings.ClientIP] = true
+	configIpList := make(map[string]bool)
+	configIpList[Settings.ClientIP] = true
 
-        // Setup TLS and start listener
-        config := MustGetTlsConfiguration(Settings.TLSCommonCA, Settings.TLSMyCert, Settings.TLSMyKey)
-        listener, err := tls.Listen("tcp", Settings.ServerIPPort, config)
-        defer listener.Close()
-        if err != nil {
-                log.Fatal(err)
-        }
+	// Setup TLS and start listener
+	config := MustGetTlsConfiguration(Settings.TLSCommonCA, Settings.TLSMyCert, Settings.TLSMyKey)
+	listener, err := tls.Listen("tcp", Settings.ServerIPPort, config)
+	defer listener.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        // Setup RPC
-        server := rpc.NewServer()
-        cal := new(RPCMethods)
-        server.Register(cal)
-        server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+	// Setup RPC server
+	server := rpc.NewServer()
+	cal := new(RPCMethods)
+	server.Register(cal)
+	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
 
-        // RPC Handle connection from client
-        for {
-                conn, err := listener.Accept()
-                defer conn.Close()
-                if err != nil {
-                        log.Fatal(err)
-                }
+	//https://groups.google.com/d/topic/golang-nuts/l09ZJQa5Cnk
+	//https://gist.github.com/2232102
+	//https://gist.github.com/2233075
+	//https://gist.github.com/1866829
 
-                //https://groups.google.com/d/topic/golang-nuts/l09ZJQa5Cnk
-                //https://gist.github.com/2232102
-                //https://gist.github.com/2233075
-                //https://gist.github.com/1866829
+	// Handle connection from client
+	for {
+		conn, err := listener.Accept()
+		defer conn.Close()
+		if err != nil {
+			log.Println(err)
+		}
 
-                tlscon, ok := conn.(*tls.Conn)
-                remoteCertIP := ""
-                if ok {
-                        //log.Print("server: conn: type assert to TLS succeedded")
-                        err := tlscon.Handshake()
-                        if err != nil {
-                                log.Fatalf("server: handshake failed: %s", err)
-                        }
-                        //log.Print("server: conn: Handshake completed")
+		tlscon, ok := conn.(*tls.Conn)
+		if ok {
+			if global_DebugLevel == "1" {
+				log.Println("Server: conn: type assert to TLS succeedded")
+			}
+			err := tlscon.Handshake()
+			if err != nil {
+				conn.Close()
+				log.Println("Server: handshake failed: %s", err)
+			}
+			state := tlscon.ConnectionState()
+			if global_DebugLevel == "1" {
+				log.Print("Server: conn: Handshake completed")
+				log.Printf("Client: Requested server name: %v\n", state.ServerName)
+				log.Printf("Client: Cert CommonName: %v\n", state.PeerCertificates[0].Subject.CommonName)
+				log.Printf("Client: Cert DNS Names %v\n", state.PeerCertificates[0].DNSNames)
+				log.Printf("Client: Cert Email Addresses %v\n", state.PeerCertificates[0].EmailAddresses)
+				log.Printf("Client: Cert IP Addresses %v\n", state.PeerCertificates[0].IPAddresses)
+			}
 
-                        state := tlscon.ConnectionState()
-                        //log.Println("Requested ServerName: ",  state.ServerName )
-                        //log.Println( state.PeerCertificates[0].Subject.CommonName )
-                        //log.Println( state.PeerCertificates[0].DNSNames )
-                        //log.Println( state.PeerCertificates[0].EmailAddresses )
-                        //log.Println( state.PeerCertificates[0].IPAddresses )
-                        //log.Println("Server: client public key is:")
-                        remoteCertIP = state.PeerCertificates[0].IPAddresses[0].String()
+			if global_DebugLevel == "3" {
+				// Use this to debug RPC method called and params provided
+				buf := make([]byte, 512)
+				for {
+					log.Print("Server: conn: waiting")
+					n, err := conn.Read(buf)
+					if err != nil {
+						if err != nil {
+							log.Printf("Server: conn: read: %s", err)
+						}
+						break
+					}
+					log.Printf("Server: conn: echo %q\n", string(buf[:n]))
+					n, err = conn.Write(buf[:n])
+					log.Printf("Server: conn: wrote %d bytes", n)
+					if err != nil {
+						log.Printf("Server: write: %s", err)
+						break
+					}
+				}
+				log.Printf("Server: Debug 3 completed. Quitting.")
+				return
+			}
 
-                        /*
-                                for _, v := range state.PeerCertificates {
-                                        log.Print(  x509.MarshalPKIXPublicKey(v.PublicKey) )
-                                }
-                                buf := make([]byte, 512)
-                                // Use this to debug RPC method called and params provided
-                                for {
-                                        log.Print("server: conn: waiting")
-                                        n, err := conn.Read(buf)
-                                        if err != nil {
-                                                if err != nil {
-                                                        log.Printf("server: conn: read: %s", err)
-                                                }
-                                                break
-                                        }
-                                        log.Printf("server: conn: echo %q\n", string(buf[:n]))
-                                        n, err = conn.Write(buf[:n])
-                                        log.Printf("server: conn: wrote %d bytes", n)
-                                        if err != nil {
-                                                log.Printf("server: write: %s", err)
-                                                break
-                                        }
-                                }
-                        */
-                }
-
-                clientIpPort := conn.RemoteAddr().String()
-                clientIp, _, err := net.SplitHostPort(clientIpPort)
-                if err != nil {
-                        conn.Close()
-                        log.Fatal(err)
-                }
-                //log.Printf("client: [%s]   remote: [%s]\n",clientIp,remoteCertIP)
-                if clientIp != remoteCertIP {
-                        log.Printf("Rejected connection from: %v\n", clientIpPort)
-                        conn.Close()
-                } else if configIpList[clientIp] {
-                        //log.Printf("Accepted connection from: %v\n", clientIpPort)
-                        go server.ServeCodec(jsonrpc.NewServerCodec(conn))
-                } else {
-                        log.Printf("Rejected connection from: %v\n", clientIpPort)
-                        conn.Close()
-                }
-        }
+			clientIpPort := conn.RemoteAddr().String()
+			clientIp, _, err := net.SplitHostPort(clientIpPort)
+			if err != nil {
+				conn.Close()
+				log.Printf("Server: Error getting client IP: %v\n", err)
+			}
+			if clientIp != state.PeerCertificates[0].IPAddresses[0].String() {
+				conn.Close()
+				log.Printf("Server: Rejected connection from: %v\n", clientIpPort)
+			}
+			if configIpList[clientIp] {
+				if global_DebugLevel == "1" {
+					log.Printf("Server: Accepted connection from: %v\n", clientIpPort)
+				}
+				go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+			} else {
+				conn.Close()
+				log.Printf("Server: Rejected connection from: %v\n", clientIpPort)
+			}
+		} else {
+			//There was a problem with the TLS connection dropping client
+			conn.Close()
+			log.Println("Server: TLS conn error: %s", err)
+		}
+	}
 }
